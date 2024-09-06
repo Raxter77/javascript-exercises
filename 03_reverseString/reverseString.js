2aa56286d1c55edce00be241820467a919b08334
raxter77 <178403681+Raxter77@users.noreply.github.com>
2024-09-06 19:48:30 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let s = String(string);
+    let newString = "";
 
+    for (c = s.length - 1; c >= 0; c--)
+    {
+        newString = newString.concat(s[c]);
+    }
+    return newString;
 };
 
 // Do not edit below this line
