b9500b5ac8b51e6eeeb838d17acc264481b72796
raxter77 <178403681+Raxter77@users.noreply.github.com>
2024-09-09 09:35:23 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,41 @@
-const sumAll = function() {
+const sumAll = function(rangeExtentA, rangeExtentB)
+{
+    if (typeof(rangeExtentA) != "number" || typeof(rangeExtentB) != "number")   
+    {
+        return "ERROR";
+    }
+    else if (!(Number.isInteger(rangeExtentA) && Number.isInteger(rangeExtentB)))
+    {
+        return "ERROR";
+    }
+    else if (rangeExtentA < 0 || rangeExtentB < 0)
+    {
+        return "ERROR";
+    }
+    else
+    {
+        let start;
+        let end;
+        if (rangeExtentA < rangeExtentB)
+        {
+            start = rangeExtentA;
+            end = rangeExtentB;
+        }
+        else
+        {
+            start = rangeExtentB;
+            end = rangeExtentA;
+        }
 
+        let total = 0;
+
+        for (let i = start; i <= end; i++)
+        {
+            total += i;
+        }
+
+        return total;
+    }
 };
 
 // Do not edit below this line
